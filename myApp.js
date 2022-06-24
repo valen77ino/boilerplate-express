59470afa9c48785476d094a0d73273e663ecc89d
Valentín Carrera <valentincarrera@yahoo.com>
2022-06-24 09:55:59 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -1,13 +1,19 @@
 let express = require('express');
 let app = express();
+
 console.log("Hello World");
+
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/",(req,res)=>
   {
     res.sendFile(__dirname + "/views/index.html")
   });
 
-
+app.get("/json",(req,res)=>
+  {
+    res.json({"message": "Hello json"})
+  });
 
 
 
