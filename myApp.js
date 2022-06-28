e23a154e63262d3b2140fa02a34798e4f9e6b323
Valentín Carrera <valentincarrera@yahoo.com>
2022-06-28 06:17:00 +0000

    Pasar parámetros en llamada.

--- myApp.js
+++ myApp.js
@@ -25,20 +25,20 @@ app.get("/json",(req,res)=>
     }
   });
 
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({"time": req.time});
+});
 
+app.get('/:word/echo', function(req, res) {
+  res.json({"echo": req.params.word});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/name', function(req, res) {
+  res.json({"name": req.query.first + " " + req.query.last});
+});
 
 
 
